58b410dbbcf29f015cc4ff26481a073a46c43cc6
kavindu senevirarthna <it20009540@my.sliit.lk>
2021-08-27 21:10:40 +0530

    TLC PROJECT

---
+++ TLC_Project/TLC_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TLC_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
