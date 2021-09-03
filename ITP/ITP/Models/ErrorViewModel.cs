3738a0ccede64538ab6964403b476736ea7607e2
kavindu senevirarthna <87372452+kavisliit@users.noreply.github.com>
2021-09-03 21:14:16 +0530

    Add files via upload
    DD

---
+++ ITP/ITP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
