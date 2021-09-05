6e393b1aa2639a8658e70efa6d76e8e8be372237
kavindu senevirarthna <it20009540@my.sliit.lk>
2021-09-05 20:08:00 +0530

    chnage

--- ITP/ITP/Controllers/HomeController.cs
+++ ITP/ITP/Controllers/HomeController.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using ITP.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ITP.Controllers
 {
@@ -20,6 +22,33 @@ namespace ITP.Controllers
 
         public IActionResult Index()
         {
+            int cid = 0;
+            String action1, action2, icon, action3, action4 = null;
+
+            if (HttpContext.Session.GetString("customersession") != null)
+            {
+
+                cid = JsonConvert.DeserializeObject<int>(HttpContext.Session.GetString("customersession"));
+                action1 = "PROFILE";
+                action2 = "LOGOUT";
+                icon = "fa-power-off";
+                action3 = "userprofile";
+
+            }
+            else
+            {
+                action1 = "LOGIN";
+                action2 = "SIGN UP";
+                icon = "fa-user-plus";
+                action3 = "Customerlogin";
+                action4 = "Register";
+            }
+            ViewData["action3"] = action3;
+            ViewData["action4"] = action4;
+            ViewData["icon"] = icon;
+            ViewData["action1"] = action1;
+            ViewData["action2"] = action2;
+            ViewData["cid"] = cid;
             return View();
         }
 
