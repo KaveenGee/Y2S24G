7392501d91afaa76d4b67deb8487eb95b5b76fcd
pramudnadula <it20168568@my.sliit.lk>
2021-09-10 23:48:26 +0530

    cart changes

---
+++ ITP/ITP/Models/OrderDetails.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITP.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Order")]
+        public int OrderId { get; set; }
+
+        [Display(Name = "Product")]
+        public int ItemId { get; set; }
+
+        [Display(Name = "Customer Id")]
+        public int CustomerId { get; set; }
+
+
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+
+        [ForeignKey("ItemId")]
+        public ItemModel Item { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer Customer{ get; set; }
+
+    }
+}
